8524692ec520c1875d0d153350ced0feaa3a2186
xxh_qh <15850781776@163.com>
2017-08-01 18:28:49 +0800

    mo

---
+++ README
@@ -0,0 +1,12 @@
+使用说明
+预测模式
+if __name__ == '__main__':
+    strategy_run('predict.py', 'predict', predict_format)
+
+交易模式
+if __name__ == '__main__':
+    strategy_run('trade.py', 'trade')
+
+
+在策略文件的目录下新建策略测试文件
+from zijin_basic import strategy_run
--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(name='zijin',
       version='1.0',
-      decription='zijin test package',
+      description='zijin test package',
       url='https://github.com/xxh13/zijin_.git',
-      author='Xuxiaohu',
+      author='xxh13',
       author_email='15850781776@163.com',
       license='MIT',
       packages=['zijin'],
+      install_requires = ['bson'],
       zip_safe=False
 )
